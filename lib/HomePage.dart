a634c734b1286e83605c38c010e2dc0b2de5141f
mehrdad <mehrdadmmb2@gmail.com>
2019-06-29 13:07:27 +0330

    new ui

---
+++ lib/HomePage.dart
@@ -0,0 +1,101 @@
+import 'package:First/Search.dart';
+import 'package:First/Visit.dart';
+import 'package:First/Wallet.dart';
+import 'package:flutter/material.dart';
+
+import 'main.dart';
+
+class HomePage extends State<HomeState> {
+  int currentPage = 0;
+
+  final List children = [
+    new Wallet(),
+    new Search(),
+    new Visit(),
+    new Search(),
+    new Wallet(),
+  ];
+
+  final appbar = new AppBar(
+    centerTitle: true,
+    backgroundColor: Colors.grey[100],
+    leading: new IconButton(
+      icon: Icon(Icons.menu),
+      onPressed: null,
+      tooltip: 'Touch to open menu',
+    ),
+    title: new Text(
+      'دهکده هوشمند',
+      textAlign: TextAlign.center,
+      style: TextStyle(color: Colors.black),
+    ),
+    actions: <Widget>[
+      new IconButton(
+        icon: Icon(Icons.message),
+        onPressed: () {},
+        color: Colors.grey,
+        tooltip: 'Touch to open message inbox',
+      )
+    ],
+  );
+
+  onItemTapped(int index) {
+    setState(() {
+      currentPage = index;
+      print(children[index]);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: new Directionality(
+          textDirection: TextDirection.rtl,
+          child: new Scaffold(
+              appBar: appbar,
+              body: this.children[currentPage],
+              bottomNavigationBar: new BottomNavigationBar(
+                items: const <BottomNavigationBarItem>[
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.home),
+                    title: Text(
+                      'صفحه اصلی',
+                      style: TextStyle(
+                          color: Colors.grey, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.attach_money),
+                    title: Text('کیف پول',
+                        style: TextStyle(
+                            color: Colors.grey, fontWeight: FontWeight.bold)),
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.card_travel),
+                    title: Text('سفرها',
+                        style: TextStyle(
+                            color: Colors.grey, fontWeight: FontWeight.bold)),
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.shop),
+                    title: Text('فروشگاه ها',
+                        style: TextStyle(
+                            color: Colors.grey, fontWeight: FontWeight.bold)),
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.hotel),
+                    title: Text('اقامتگاه ها',
+                        style: TextStyle(
+                            color: Colors.grey, fontWeight: FontWeight.bold)),
+                  ),
+                ],
+                currentIndex: currentPage,
+                selectedItemColor: Colors.green[500],
+                unselectedItemColor: Colors.grey,
+                onTap: onItemTapped,
+                showUnselectedLabels: true,
+              )),
+        ));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,74 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:First/HomePage.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(HomeState());
 
-class MyApp extends StatelessWidget {
-
+class HomeState extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: new Directionality(
-          textDirection: TextDirection.rtl,
-          child: new Scaffold(
-            appBar: AppBar(
-              backgroundColor: Colors.grey[100],
-              leading: new IconButton(
-                icon: Icon(Icons.menu),
-                onPressed: null,
-                tooltip: 'Touch to open menu',
-              ),
-              title: new Text(
-                'دهکده هوشمند',
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.black),
-              ),
-              actions: <Widget>[
-                new IconButton(
-                  icon: Icon(Icons.message),
-                  onPressed: null,
-                  tooltip: 'Touch to open message inbox',
-                )
-              ],
-            ),
-            body: new Center(
-              child: new Text(
-                'محتوای برنامه',
-                style: TextStyle(fontSize: 25.0),
-              ),
-            ),
-            bottomNavigationBar: BottomNavigationBar(
-              items: const <BottomNavigationBarItem>[
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.home),
-                  title: Text('صفحه اصلی',style: TextStyle(color: Colors.grey,fontWeight: FontWeight.bold),),
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.attach_money),
-                  title: Text('کیف پول',style: TextStyle(color: Colors.grey,fontWeight: FontWeight.bold)),
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.card_travel),
-                  title: Text('سفرها',style: TextStyle(color: Colors.grey,fontWeight: FontWeight.bold)),
-                ),
-                 BottomNavigationBarItem(
-                  icon: Icon(Icons.shop),
-                  title: Text('فروشگاه ها',style: TextStyle(color: Colors.grey,fontWeight: FontWeight.bold)),
-                ),
-                 BottomNavigationBarItem(
-                  icon: Icon(Icons.hotel),
-                  title: Text('اقامتگاه ها',style: TextStyle(color: Colors.grey,fontWeight: FontWeight.bold)),
-                ),
-              ],
-              currentIndex: 0,
-              selectedItemColor: Colors.green[500],
-              unselectedItemColor: Colors.grey,
-              onTap: null,
-              showUnselectedLabels: true,
-            ),
-          ),
-        ));
-        
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return new HomePage();
   }
-  
 }
+
+
